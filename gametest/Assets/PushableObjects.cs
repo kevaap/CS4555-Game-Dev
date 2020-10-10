e8052821bfdafae9d62b4ace7cd324978b88e076
kevaap <kaguile6@calstatela.edu>
2020-10-09 21:34:28 -0700

    added interactions to objects

---
+++ gametest/Assets/PushableObjects.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reflection;
+using System.Threading;
+using UnityEngine;
+
+public class PushableObjects : MonoBehaviour
+{
+    public float pushStrength = 6.0f;
+    
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        if (hit.moveDirection.y < -0.3f)
+        {
+            return;
+        }
+
+
+        Vector3 direction = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = direction * pushStrength;
+    }
+
+}
