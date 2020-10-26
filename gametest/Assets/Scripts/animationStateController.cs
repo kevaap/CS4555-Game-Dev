90b1c89d4b809131542355a3d3e9ae127a8f77df
WizardMode <humanspacebolt@live.com>
2020-10-26 09:39:40 -0700

    jumping and gravity fixed

--- gametest/Assets/Scripts/animationStateController.cs
+++ gametest/Assets/Scripts/animationStateController.cs
@@ -5,97 +5,97 @@ using UnityEngine.InputSystem;
 
 public class animationStateController : MonoBehaviour
 {
-    Animator animator;
+  Animator animator;
 
-    void Start()
+  void Start()
+  {
+      animator = GetComponent<Animator>();
+  }
+
+  void Update()
+  {
+
+    var keyboard = Keyboard.current;
+
+    bool isWalking = animator.GetBool("isWalking");
+    //bool isJumping = animator.GetBool("isJumping");
+    bool isAttacking = animator.GetBool("isAttacking");
+    bool isTurningL = animator.GetBool("isTurningL");
+    bool isTurningR = animator.GetBool("isTurningR");
+    bool isDucking = animator.GetBool("isDucking");
+
+
+    bool wPressed = keyboard.wKey.isPressed; //walk
+    bool aPressed = keyboard.aKey.isPressed; //turn left
+    bool sPressed = keyboard.sKey.isPressed; //duck
+    bool dPressed = keyboard.dKey.isPressed; //turn right
+    bool iPressed = keyboard.iKey.isPressed; //attack
+    //bool spacePressed = keyboard.spaceKey.isPressed; //jump
+
+    if(keyboard != null)
     {
-        animator = GetComponent<Animator>();
-    }
+      if(!isWalking && wPressed )
 
-    void Update()
-    {
-      var keyboard = Keyboard.current;
-
-      bool isWalking = animator.GetBool("isWalking");
-      bool isJumping = animator.GetBool("isJumping");
-      bool isAttacking = animator.GetBool("isAttacking");
-      bool isTurningL = animator.GetBool("isTurningL");
-      bool isTurningR = animator.GetBool("isTurningR");
-      bool isDucking = animator.GetBool("isDucking");
-
-
-      bool wPressed = keyboard.wKey.isPressed; //walk
-      bool aPressed = keyboard.aKey.isPressed; //turn left
-      bool sPressed = keyboard.sKey.isPressed; //duck
-      bool dPressed = keyboard.dKey.isPressed; //turn right
-      bool iPressed = keyboard.iKey.isPressed; //attack
-
-      bool spacePressed = keyboard.spaceKey.isPressed;
-
-      if(keyboard != null)
       {
-        if(!isWalking && wPressed )
+        animator.SetBool("isWalking", true);
+      }
 
-        {
-          animator.SetBool("isWalking", true);
-        }
+      if(isWalking && !wPressed)
 
-        if(isWalking && !wPressed)
+      {
+        animator.SetBool("isWalking", false);
+      }
+      /*
+      if(!isJumping && spacePressed)
+      {
+        animator.SetBool("isJumping", true);
+      }
 
-        {
-          animator.SetBool("isWalking", false);
-        }
+      if(isJumping && !spacePressed)
+      {
+        animator.SetBool("isJumping", false);
+      }
+      */
+      if(!isAttacking && iPressed)
+      {
+        animator.SetBool("isAttacking", true);
+      }
 
-        if(!isJumping && spacePressed)
-        {
-          animator.SetBool("isJumping", true);
-        }
+      if(isAttacking && !iPressed)
+      {
+        animator.SetBool("isAttacking", false);
+      }
 
-        if(isJumping && !spacePressed)
-        {
-          animator.SetBool("isJumping", false);
-        }
+      if(!isTurningL && aPressed)
+      {
+        animator.SetBool("isTurningL", true);
+      }
 
-        if(!isAttacking && iPressed)
-        {
-          animator.SetBool("isAttacking", true);
-        }
+      if(isTurningL && !aPressed)
+      {
+        animator.SetBool("isTurningL", false);
+      }
 
-        if(isAttacking && !iPressed)
-        {
-          animator.SetBool("isAttacking", false);
-        }
+      if(!isTurningR && dPressed)
+      {
+        animator.SetBool("isTurningR", true);
+      }
 
-        if(!isTurningL && aPressed)
-        {
-          animator.SetBool("isTurningL", true);
-        }
+      if(isTurningR && !dPressed)
+      {
+        animator.SetBool("isTurningR", false);
+      }
+      if(!isDucking && sPressed)
+      {
+        animator.SetBool("isDucking", true);
+      }
 
-        if(isTurningL && !aPressed)
-        {
-          animator.SetBool("isTurningL", false);
-        }
+      if(isDucking && !sPressed)
+      {
+        animator.SetBool("isDucking", false);
+      }
 
-        if(!isTurningR && dPressed)
-        {
-          animator.SetBool("isTurningR", true);
-        }
+  }
 
-        if(isTurningR && !dPressed)
-        {
-          animator.SetBool("isTurningR", false);
-        }
-
-        if(!isDucking && sPressed)
-        {
-          animator.SetBool("isDucking", true);
-        }
-
-        if(isDucking && !sPressed)
-        {
-          animator.SetBool("isDucking", false);
-        }
-    }
-
-    }
+  }
 }
