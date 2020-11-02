783cd2e5d9da4d92a9aae6b49f22e1664996a775
WizardMode <humanspacebolt@live.com>
2020-11-01 22:52:58 -0800

    testing

--- gametest/Assets/playerMovement.cs
+++ gametest/Assets/playerMovement.cs
@@ -1,41 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+  public CharacterController controller;
 
-    float speed = 2.5f;
-    float speedRotate = 75.0f;
+  public float speed = 5f;
+  public float gravity = -9.81f;
+  public float jumpHeight = 3f;
 
-    // Update is called once per frame
-    void Update()
+  public Transform groundCheck;
+  public float groundDistance = 0.4f;
+  public LayerMask groundMask;
+
+  Vector3 velocity;
+  bool isGrounded;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+    if(isGrounded && velocity.y < 0)
     {
-        var keyboard = Keyboard.current;
-        if (keyboard != null)
-        {
-            if (keyboard.wKey.isPressed)
-            {
-                Vector3 movement = new Vector3(0.0f, 0.0f, 1.0f * Time.deltaTime * speed);
-                movement = transform.TransformDirection(movement);
-                controller.Move(movement);
-            }
-            if (keyboard.aKey.isPressed)
-            {
-                Vector3 rotation = new Vector3(0.0f, -1.0f * Time.deltaTime * speedRotate, 0.0f);
-                transform.Rotate(rotation);
-            }
-            if (keyboard.sKey.isPressed)
-            {
-                Vector3 movement = new Vector3(0.0f, 0.0f, -1.0f * Time.deltaTime * speed);
-                movement = transform.TransformDirection(movement);
-                controller.Move(movement);
-            }
-            if (keyboard.dKey.isPressed)
-            {
-                Vector3 rotation = new Vector3(0.0f, 1.0f * Time.deltaTime * speedRotate, 0.0f);
-                transform.Rotate(rotation);
-            }
-        }
+      velocity.y = -2f;
     }
+    float x = Input.GetAxis("Horizontal");
+    float z = Input.GetAxis("Vertical");
+
+    Vector3 move = transform.right * x + transform.forward * z;
+
+    //controller.Move(move * speed * Time.deltaTime);
+
+    if(Input.GetButtonDown("Jump") && isGrounded)
+    {
+      velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+    velocity.y += gravity * Time.deltaTime;
+
+    controller.Move(velocity * Time.deltaTime);
+  }
 }
