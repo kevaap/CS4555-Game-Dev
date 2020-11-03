5315e606e6e239c8602ce73227199929cb6203fb
mrefuerzo001 <mrefuerzo001@yahoo.com>
2020-11-02 20:11:24 -0800

    implemented doorTrigger and improved gravity

---
+++ gametest/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    bool isOpened = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+            door.transform.position += new Vector3(0, 5, 0);
+        }
+           
+    }
+}
---
+++ gametest/Assets/Scripts/robotBreak.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotBreak : MonoBehaviour
+{
+  Animator animator;
+
+void Start()
+{
+  animator = GetComponent<Animator>();
+}
+
+
+private void OnTriggerEnter(Collider other)
+{
+      if(other.tag == "reach")
+      {
+
+          animator.SetBool("break",true);
+      }
+    }
+
+}
