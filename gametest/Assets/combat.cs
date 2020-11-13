1fb20445f71b40d18c4c846e7e347b2bfb5bc6ce
WizardMode <humanspacebolt@live.com>
2020-11-13 06:36:44 -0800

    robot-shoot & teddy-health

--- gametest/Assets/combat.cs
+++ gametest/Assets/combat.cs
@@ -1,19 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class combat : MonoBehaviour
 {
+    // heath stats
     public float maxHealth;
     public float health;
+
+    // UI objects
+    // to display enemy health
+    public GameObject healthUI;
+    public Slider sliderHealth;//
 
     void Start()
     {
       health = maxHealth;
+      sliderHealth.value = CalculateHealth();
     }
+
+    void Update()
+    {
+      sliderHealth.value = CalculateHealth();
+
+      if(health < maxHealth)
+      {
+        healthUI.SetActive(true);
+      }
+
+      if(health > maxHealth)
+      {
+        health = maxHealth;
+      }
+    }
+
+    float CalculateHealth()
+    {
+      return health / maxHealth;
+    }
+
     public void TakeDamage(float amount)
     {
       health -= amount;
+
+      sliderHealth.value = CalculateHealth();
+
       if(health <= 0)
       {
         health = 0;
