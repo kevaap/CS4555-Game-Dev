2d3b3f4e6e7ccf9daba1a81cb21c2fa76d60dca1
WizardMode <humanspacebolt@live.com>
2020-11-13 07:10:45 -0800

    power-up health

---
+++ gametest/Assets/PowerHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerHealth : MonoBehaviour
+{
+  public float add_health = 100;
+
+  void OnTriggerEnter (Collider other)
+  {
+    if (other.CompareTag("Player"))
+    {
+    Pickup(other);
+    }
+  }
+
+
+  void Pickup(Collider player)
+  {
+
+    combat stats = player.GetComponent<combat>();
+    stats.health += add_health;
+
+    //GetComponent<MeshRenderer>().enabled = false;
+    //GetComponent<Collider>().enabled = false;
+
+    Destroy(gameObject);
+  }
+}
